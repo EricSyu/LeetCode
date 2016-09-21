243dedd42cef73278e239486f4f070be677e8c8e
WeiJhe <sky820902@gmail.com>
2016-09-21 11:01:00 +0800

    update the code of 001_TwoSum

--- Algorithms/001_TwoSum.cpp
+++ Algorithms/001_TwoSum.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> map;
         vector<int> indices;
-        for(unsigned int i=0; i<nums.size(); i++){
-            for(unsigned int j=i+1; j<nums.size(); j++){
-                if(nums.at(i)+nums.at(j) == target){
-                    indices.push_back(i);
-                    indices.push_back(j);
-                    return indices;
-                }
+        for(int i=0; i<nums.size(); i++){
+            map.insert(pair<int,int>(nums.at(i),i));
+        }
+        for(int i=0; i<nums.size(); i++){
+            int c = target-nums.at(i);
+            if(map.find(c)!=map.end() && map.at(c)!=i){
+                indices.push_back(i);
+                indices.push_back(map.at(c));
+                break;
             }
         }
+        
         return indices;
     }
 };
